c25a876822f26f04dec3259d1e808dec2d40f04f
Klemens Käsznar <klemens.kaesznar@gmail.com>
2022-04-25 16:32:27 +0200

    Initial commit

---
+++ src/main/java/at/kk/msc/hcov/core/PlatformCore.java
@@ -0,0 +1,13 @@
+package at.kk.msc.hcov.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformCore {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformCore.class, args);
+	}
+
+}
---
+++ src/test/java/at/kk/msc/hcov/core/PlatformCoreTests.java
@@ -0,0 +1,13 @@
+package at.kk.msc.hcov.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformCoreTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
