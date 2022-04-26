049204e6b138ebde9bf4975f2e954fb3a5afb6c8
Klemens Käsznar <klemens.kaesznar@gmail.com>
2022-04-26 12:00:55 +0200

    Adapt interface and add documentation

--- src/main/java/at/kk/msc/hcov/core/service/ontology/metrics/IOntologyMetrics.java
+++ src/main/java/at/kk/msc/hcov/core/service/ontology/metrics/IOntologyMetrics.java
@@ -1,9 +1,20 @@
 package at.kk.msc.hcov.core.service.ontology.metrics;
 
 import java.util.Map;
+import org.apache.jena.ontology.OntModel;
 
+/**
+ * Interface for implementing classes that supply general information about ontologies.
+ */
 public interface IOntologyMetrics {
 
-  Map<String, String> calculateMetrics();
+
+  /**
+   * Calculates the targeted metrics of an ontology.
+   *
+   * @param ontModel the ontology for which the metric shall be created.
+   * @return a Map containing the metric names and values.
+   */
+  Map<String, String> calculateMetrics(OntModel ontModel);
 
 }
--- src/main/java/at/kk/msc/hcov/core/service/ontology/metrics/stub/IOntologyMetricsStub.java
+++ src/main/java/at/kk/msc/hcov/core/service/ontology/metrics/stub/IOntologyMetricsStub.java
@@ -3,13 +3,14 @@ package at.kk.msc.hcov.core.service.ontology.metrics.stub;
 import at.kk.msc.hcov.core.service.ontology.metrics.IOntologyMetrics;
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.jena.ontology.OntModel;
 import org.springframework.stereotype.Component;
 
 @Component
 public class IOntologyMetricsStub implements IOntologyMetrics {
 
   @Override
-  public Map<String, String> calculateMetrics() {
+  public Map<String, String> calculateMetrics(OntModel ontModel) {
     return new HashMap<>();
   }
 
