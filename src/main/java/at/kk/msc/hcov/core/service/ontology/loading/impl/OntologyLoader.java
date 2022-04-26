4d4b6444d2df541e2f5a0615c437d6e7764b6a68
Klemens Käsznar <klemens.kaesznar@gmail.com>
2022-04-26 11:24:15 +0200

    Add logging

--- src/main/java/at/kk/msc/hcov/core/service/ontology/loading/impl/OntologyLoader.java
+++ src/main/java/at/kk/msc/hcov/core/service/ontology/loading/impl/OntologyLoader.java
@@ -6,18 +6,24 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OntologyLoader implements IOntologyLoader {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(OntologyLoader.class);
+
   @Override
   public OntModel loadOntology(String path) throws FileNotFoundException {
+    LOGGER.info("Started loading ontology from: {}", path);
     OntModel ontModel = ModelFactory.createOntologyModel();
 
     InputStream inputStream = new FileInputStream(path);
     ontModel.read(inputStream, null);
-
+    LOGGER.info("Loaded ontology from: {}", path);
+    
     return ontModel;
   }
 
