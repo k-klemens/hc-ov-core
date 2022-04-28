24bfc77e391686a9065270bcb42a8c9e0c4eb145
Klemens Käsznar <klemens.kaesznar@gmail.com>
2022-04-28 11:13:10 +0200

    Add documentation

--- src/main/java/at/kk/msc/hcov/core/service/plugin/IPluginLoader.java
+++ src/main/java/at/kk/msc/hcov/core/service/plugin/IPluginLoader.java
@@ -2,8 +2,21 @@ package at.kk.msc.hcov.core.service.plugin;
 
 import at.kk.msc.hcov.core.service.exception.PluginLoadingError;
 
+/**
+ * Interface to specify the how plugins can be loaded.
+ *
+ * @param <P>
+ */
 public interface IPluginLoader<P> {
 
+  /**
+   * Load a plugin if available or throws exception.
+   *
+   * @param type     the type of the plugin to load
+   * @param pluginId the id of the plugin to load
+   * @return the plugin if available
+   * @throws PluginLoadingError if something went wrong when loading the plugin
+   */
   P loadPluginOrThrow(PluginType type, String pluginId) throws PluginLoadingError;
 
   enum PluginType {
--- src/main/java/at/kk/msc/hcov/core/service/templating/ITemplatingService.java
+++ src/main/java/at/kk/msc/hcov/core/service/templating/ITemplatingService.java
@@ -8,8 +8,21 @@ import java.util.Map;
 import java.util.UUID;
 import org.apache.jena.ontology.OntModel;
 
+/**
+ * Specifies interface which is capable of populating templates to create HTML documents to be used for the verifications.
+ */
 public interface ITemplatingService {
 
+  /**
+   * Creates HTML documents reflecting the verification tasks for all passed model elements.
+   *
+   * @param extractedModelElementsMap map containting the extracted model elements to for each a HTML document shall be created.
+   * @param providedContextMap        map containing possible context objects to the model elements. Can be an empty map if the context is not needed.
+   * @param verificationTaskPlugin    plugin used to create the verificaiton task to provide the template as well as the template variables.
+   * @param specification             specification of the verification to obtain configuration values.
+   * @return a Map of HTML documents reflecting the individual verification tasks keyed by the extracted model element ids.
+   * @throws PluginConfigurationNotSetException if the verificationTaskPlugin requires a configuration the configuration was not set properly
+   */
   Map<UUID, String> populateTemplates(
       Map<UUID, OntModel> extractedModelElementsMap,
       Map<UUID, ProvidedContext> providedContextMap,
